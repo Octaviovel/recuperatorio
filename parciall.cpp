e6d0aa0a10955807a12c117d1528bb568dad1196
ovelardez <velardezoctavio456@gmail.com>
2024-09-20 16:23:22 -0300

    lalala

---
+++ parciall.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int cantidadDeMaterias, nombre, nota, ponderacion;
+    int sumaDeLaPonderacion = 0;
+    int promedioPonderado = 0;
+
+    cout << "ingrese la cantidad de materias" << endl;
+    cin >> cantidadDeMaterias;
+
+    for(int i=1; i<=cantidadDeMaterias; i++){
+        cout << "ingrese el nombre de la materia" << endl;
+        cin >> nombre;
+        cout << "ingrese la nota de la materia" << endl;
+        cin >> nota;
+
+        cout << "ingrese la ponderacion de la materia" << endl;
+        cin >> ponderacion;
+
+        cout << "materia " << i + 1 << ":" << endl;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
